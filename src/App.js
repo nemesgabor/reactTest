5bbb25cf9ca0c7dc56986dcfe8d18ec5f3e0e471
Gabor Nemes <Gabor.Nemes@arm.com>
2018-04-18 17:51:57 +0200

    Section 3, Lecture 42

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Test 1", age: 32},
+      { name: "Test 2", age: 34},
+      { name: "Test 3", age: 36}            
+    ]
+  }
+  switchNameHandler = () => {
+    //console.log('Clicked');
+    // this.state.persons[0].name = "Test qwe"; DONT
+    this.setState({persons:[
+        { name: "Test 1", age: 32},
+        { name: "Test 2", age: 34},
+        { name: "Test 3", age: 21}            
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>Test text</p>
-        <Person name="Test 1" age="32" />
-        <Person name="Test 2" age="34" >My hobbies Racing</Person>
-        <Person name="Test 3" age="36" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>        
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
         
       </div>
     );
